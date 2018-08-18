4727669a9d8a33ab3374219302c4696c0ac4c321
Jeff McGinley <jeff.mcginley.indy@gmail.com>
2018-08-18 12:33:50 -0400

    chapter 2 - added templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,25 @@
 # app/routes.py
 
+from flask import render_template
+
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    return "Hello, World!"
+    user = {'username': 'Jeffrey'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+    ]
+
+    return render_template('index.html', title='Home', user=user,
+        posts=posts)
